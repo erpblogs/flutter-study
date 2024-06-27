89fc1c513274c3e9e13bed96e92813d9f3f8cf55
quangtrinh <trinhvanquangf1@gmail.com>
2024-06-27 08:53:51 +0700

    using GeneratorPage to show a Placeholder page

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
     return Scaffold(
       body: Row(
         children: [
@@ -90,7 +102,8 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              // child: GeneratorPage(),
+              child: page, // switches between our GeneratorPage and the placeholder, that will soon become the Favorites page.
             ),
           ),
         ],
